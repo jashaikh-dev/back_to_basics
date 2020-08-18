36e6da8d70b52f52f73078ac9cd1fb7de91161cd
jashaikh-dev <junaidazharshaikh@gmail.com>
2020-08-17 21:21:58 -0400

    feat: even and odd

---
+++ JavaScript/evenOdd/index.js
@@ -0,0 +1,12 @@
+let num = 20;
+
+if (num % 2 === 0)
+  console.log(num + 'is even');
+else
+  console.log(num + 'is odd');
+
+
+console.log(  `${num}  ${(num % 2 === 0) ? 'is even' : 'is odd'}` );
+
+// Love JS
+// `> node index` to run the code
